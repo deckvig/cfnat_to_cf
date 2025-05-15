1dfeea4b9c91760f967a72949fa92bce13657b05
Deckvig <2008.zhengxing@gmail.com>
2025-05-15 08:10:40 +0000

    update

--- fresh_cloudflare.sh
+++ fresh_cloudflare.sh
@@ -2,14 +2,13 @@
 
 file=$1
 
-
-
 API_TOKEN=$(printenv CF_TOKEN)
 ZONE_ID=$(printenv CF_ZONE)
 SUBDOMAIN=$(printenv CF_DOMAIN)
 DNS_RECORD_ID=$(printenv CF_DNS_ID)
 TELEGRAM_BOT_TOKEN=$(printenv TELEGRAM_BOT_TOKEN)
 TELEGRAM_CHAT_ID=$(printenv TELEGRAM_CHAT_ID)
+INTERVAL=${INTERVAL:-90}
 
 function update_cloudflare_ip() {
     new_ip=$1
@@ -40,21 +39,32 @@ while true
 do
     echo "check ip in time interval, time: $(date)"
     echo "current ip is $ip"
-    new_ip=$(grep '选择最佳连接' $file | tail -n 1 | awk '{print $5}' | cut -d ':' -f 1)
+    new_ip=$(tail -n 10 $file | grep '选择最佳连接'|tail -n 1 | awk '{print $5}' | cut -d ':' -f 1)
     if [ -z "$new_ip" ]; then
         echo "ip not found"
-        sleep 60
+        sleep ${INTERVAL}
         continue
     fi
     if [ "$new_ip" != "$ip" ]; then
         echo "update ip to $new_ip"
-        test $(update_cloudflare_ip "$new_ip") && echo "update cloudflare ip success" && ip=$new_ip
-        test $(send_message "$new_ip") && echo "send message success"
+        if update_cloudflare_ip "$new_ip"; then
+            echo "update cloudflare ip success"
+            ip=$new_ip
+        else
+            echo "update cloudflare ip failed"
+        fi
+
+        if send_message "$new_ip"; then
+            echo "send message success"
+        else
+            echo "send message failed"
+        fi
         echo ""
     else
         echo "ip ${ip} not changed"
     fi
+
     # 定期清空日志
     truncate -s 0 $file
-    sleep 60
+    sleep ${INTERVAL}
 done
